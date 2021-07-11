19b7587debd120dc1504e2e02e4f3215e4db0e13
Adrian Quiroz <adrian.quirozpal@gmail.com>
2021-07-10 20:39:51 -0500

    finalizacion del EndPoint quoteCar

--- src/controllers/apis.js
+++ src/controllers/apis.js
@@ -30,10 +30,10 @@ const bestOptionsPerYear = async (req, res) => {
 
   //Asignacion de mejor cotizacion por categoria.
 
-  const bestRC = getBestQuote(RC);
-  const bestLow = getBestQuote(low);
-  const bestMid = getBestQuote(mid);
-  const bestHigh = getBestQuote(high);
+  const bestRC = getBestQuote(RC, false);
+  const bestLow = getBestQuote(low, false);
+  const bestMid = getBestQuote(mid, false);
+  const bestHigh = getBestQuote(high, false);
 
   res.status(200).json({
     bestRC,
@@ -44,17 +44,66 @@ const bestOptionsPerYear = async (req, res) => {
 };
 
 const quoteCar = async (req, res) => {
-  console.log("endpoint quoteCar");
+  const { brand, year, hasAC } = req.body;
+  const RC = [];
+  const low = [];
+  const mid = [];
+  const high = [];
+
+  //Busqueda de las cotizaciones por año.
+  quotes.map((quote) => {
+    if (quote.brand === brand) {
+      if (year >= quote.yearRange[0] && year <= quote.yearRange[1]) {
+        switch (quote.coverageType) {
+          case "RC":
+            RC.push(quote);
+            break;
+          case "Low":
+            low.push(quote);
+            break;
+          case "Mid":
+            mid.push(quote);
+            break;
+          case "High":
+            high.push(quote);
+
+            break;
+        }
+      }
+    }
+  });
+
+  //Asignacion de mejor cotizacion por categoria.
+
+  const bestRC = getBestQuote(RC, hasAC);
+  const bestLow = getBestQuote(low, hasAC);
+  const bestMid = getBestQuote(mid, hasAC);
+  const bestHigh = getBestQuote(high, hasAC);
+
+  res.status(200).json({
+    bestRC,
+    bestLow,
+    bestMid,
+    bestHigh,
+  });
 };
 
-const getBestQuote = (quotes) => {
+const getBestQuote = (quotes, hasAC) => {
   // Comparacion de las cotizaciones para obtener el mejor
   if (quotes.length > 0) {
     const bestQuotes = quotes.sort((a, b) => {
-      if (a.price > b.price) {
+      //suma de precio y de extra precio
+      let precioA = parseFloat(a.price);
+      let precioB = parseFloat(b.price);
+      if (hasAC) {
+        precioA += parseFloat(a.extraCoveragePrice);
+        precioB += parseFloat(b.extraCoveragePrice);
+      }
+
+      if (precioA > precioB) {
         return 1;
       }
-      if (a.price < b.price) {
+      if (precioA < precioB) {
         return -1;
       }
 
